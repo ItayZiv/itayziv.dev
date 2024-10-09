6c28e4cf502704ea059c404edad5cbb8f2f90058
Itay Ziv <itay220204@gmail.com>
2024-10-09 14:16:11 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 <div class="flex flex-col flex-wrap content-center justify-around h-full">
 	<div class="md:w-3/5">
 		<h1 class="text-6xl text-slate-200">hi i'm <span class="text-emerald-400">itay ziv</span></h1>
-		<p class="text-3xl text-slate-400 text-opacity-80 mt-8">coming soon...</p>
 	</div>
 	<div />
 </div>
